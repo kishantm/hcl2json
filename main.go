462df814919c04e53dcaef676df3707edc66fd70
kishantm <39464537+kishantm@users.noreply.github.com>
2023-03-14 14:40:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -19,54 +17,22 @@ func main() {
 	flag.BoolVar(&options.Simplify, "simplify", false, "If true attempt to simply expressions which don't contain any variables or unknown functions")
 	flag.Parse()
 
-	buffer := bytes.NewBuffer([]byte{})
-	files := flag.Args()
-	var inputName string
-
-	switch len(files) {
-	case 0:
-		files = append(files, "-")
-		inputName = "STDIN"
-	case 1:
-		inputName = files[0]
-		if inputName == "-" {
-			inputName = "STDIN"
-		}
-	default:
-		inputName = "COMPOSITE"
+	var filename = flag.Arg(0)
+	var bytes []byte
+	var err error
+	if filename == "" || filename == "-" {
+		bytes, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		bytes, err = ioutil.ReadFile(filename)
+	}
+	if err != nil {
+		logger.Fatalf("Failed to read file: %s\n", err)
 	}
 
-	for _, filename := range files {
-		var stream io.Reader
-		if filename == "-" {
-			stream = os.Stdin
-			filename = "STDIN" // for better error message
-		} else {
-			file, err := os.Open(filename)
-			if err != nil {
-				logger.Fatalf("Failed to open %s: %s\n", filename, err)
-			}
-			defer file.Close()
-			stream = file
-		}
-		_, err := buffer.ReadFrom(stream)
-		if err != nil {
-			logger.Fatalf("Failed to read from %s: %s\n", filename, err)
-		}
-		buffer.WriteByte('\n') // just in case it doesn't have an ending newline
-	}
-
-	converted, err := convert.Bytes(buffer.Bytes(), inputName, options)
+	content, err := convert.Bytes(bytes, filename, options)
 	if err != nil {
 		logger.Fatalf("Failed to convert file: %v", err)
 	}
 
-	var indented bytes.Buffer
-	if err := json.Indent(&indented, converted, "", "    "); err != nil {
-		logger.Fatalf("Failed to indent file: %v", err)
-	}
-
-	if _, err := indented.WriteTo(os.Stdout); err != nil {
-		logger.Fatalf("Failed to write to standard out: %v", err)
-	}
+	os.Stdout.Write(content)
 }
